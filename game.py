a595f1ed450faf29055ea176760802dbfcbb249c
Nikhil-Khetani <67691373+Nik-Khet@users.noreply.github.com>
2021-01-18 20:02:02 +0000

    Added assets folder

--- game.py
+++ game.py
@@ -1,5 +1,12 @@
 import pygame
 
 
-def gameloop():
-    pass
+class DinoGame(object):
+
+    def __init__(self,display_width, display_height):
+        self.DISPLAY_WIDTH = display_width
+        self.DISPLAY_HEIGHT =  display_height
+        self.DISPLAY = pygame.display.set_mode((display_width,display_height))
+        pygame.display.set_caption("Dino Game")
+        
+        self.dino_xpos
