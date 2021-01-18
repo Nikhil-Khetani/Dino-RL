2bf31b5425e7c6473546322736209d34f7cedc1d
Nikhil-Khetani <67691373+Nik-Khet@users.noreply.github.com>
2021-01-18 20:44:18 +0000

    Added Dino movement

--- game.py
+++ game.py
@@ -1,18 +1,26 @@
 import pygame
+import os
 
 
 
 class Dino(object):
-    def __init__(self, x, y):
+    def __init__(self, x, y, floor):
         self.x = x
         self.y = y
         self.image = pygame.transform.scale(pygame.image.load(os.path.join('Assets','dino.png')),(50,50))
         self.rect = self.image.get_rect()
+        self.jump=False
+        self.vel_y=0
+        self.floor = floor
 
     def step(self):
+        if self.y <= self.floor:
+            self.vel_y = 0
+        if self.jump and self.y==self.floor:
+            self.vel_y = -20
+        self.y+=self.vel_y
+        self.vel_y+=1
 
-        pass
-    
     def render(self, window):
         window.blit(self.image,(self.x,self.y))
 
@@ -21,9 +29,17 @@ class Cactus(object):
         if size=='small':
             self.x = x
             self.y = y
+            self.image = pygame.transform.scale(pygame.image.load(os.path.join('Assets','cactus.png')),(40,80))
 
     def step(self):
         self.x-=1
+        if self.x<0:
+            return True
+        else:
+            return False
+
+    def render(self, window):
+        window.blit(self.image,(self.x,self.y))
         
 
 
@@ -34,12 +50,12 @@ class DinoGame(object):
         self.DISPLAY_HEIGHT =  display_height
         self.DISPLAY = pygame.display.set_mode((display_width,display_height))
         pygame.display.set_caption("Dino Game")
-        
-        self.dino_xpos = self.DISPLAY_WIDTH/8
-        self.dino_ypos = self.DISPLAY_HEIGHT*(1-1/8)
-        self.dino_image = 
-        self.background_image = pygame.transform.scale(pygame.image.load(os.path.join('Assets','dino.png')),(self.DISPLAY_WIDTH,self.DISPLAY_HEIGHT))
+        self.cacti = []
+        self.background_image = pygame.transform.scale(pygame.image.load(os.path.join('Assets','background.png')),(self.DISPLAY_WIDTH,self.DISPLAY_HEIGHT))
+        self.frame =0
     
 
-    def nextframe(action):
+    def nextframe(self,action):
+        self.frame+=1
+        
         pass
