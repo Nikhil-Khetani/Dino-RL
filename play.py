c6a47a7359d86f0e863b4783be4cd4ceadd4dea9
Nikhil-Khetani <67691373+Nik-Khet@users.noreply.github.com>
2021-01-20 22:38:51 +0000

    Fixed play.py

--- play.py
+++ play.py
@@ -7,11 +7,11 @@ run=True
 while run:
     a=0
     for event in pygame.event.get():
-        if event == pygame.QUIT:
+        if event.type == pygame.QUIT:
             run=False
             pygame.display.quit()
             pygame.quit()
-        if event == pygame.MOUSEBUTTONUP:
+        if event.type == pygame.MOUSEBUTTONUP:
             a=1
  
     my_game.nextframe(a)
